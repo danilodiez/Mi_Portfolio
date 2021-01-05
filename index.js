a735e5c8078cfd5af303e56f96ad3441268b784e
danilodiez <danilodiez1@hotmail.com>
2021-01-05 17:03:46 -0300

    Commits finales del portfolio

---
+++ index.js
@@ -0,0 +1,36 @@
+function navResponsive() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+
+// Get the offset position of the navbar
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  var navbar = document.getElementById("myTopnav");
+  var sticky = navbar.offsetTop;
+  
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
